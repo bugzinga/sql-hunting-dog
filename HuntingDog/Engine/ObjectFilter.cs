a2660e70b2e065221b7856606a22557fbbf75b62
csbubbles <devnull@localhost>
2012-12-27 17:16:31 -0800

    Minor changes

---
+++ HuntingDog/Engine/ObjectFilter.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace DatabaseObjectSearcher
+{
+    public class ObjectFilter
+    {
+        public Boolean ShowTables
+        {
+            get;
+            set;
+        }
+
+        public Boolean ShowSP
+        {
+            get;
+            set;
+        }
+
+        public Boolean ShowViews
+        {
+            get;
+            set;
+        }
+
+        public Boolean ShowFunctions
+        {
+            get;
+            set;
+        }
+    }
+}
