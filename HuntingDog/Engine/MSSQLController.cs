ab67e090476d6d2cd7b6decec5bc214cbe477121
csbubbles <devnull@localhost>
2012-12-27 16:35:35 -0800

    MSSQLController refactored

--- HuntingDog/Engine/MSSQLController.cs
+++ HuntingDog/Engine/MSSQLController.cs
@@ -1,133 +1,47 @@
 
 using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
 using EnvDTE;
 using EnvDTE80;
 using Microsoft.SqlServer.Management.UI.VSIntegration;
-//using Microsoft.SqlServer.Management.Smo.RegSvrEnum;
+using System.Reflection;
 
 namespace DatabaseObjectSearcher
 {
     public class MSSQLController
     {
-        private static MSSQLController currentInstance = new MSSQLController();
-
-        // private DTE2 _application;
-
-        private EnvDTE.Window toolWindow;
-
-        private DatabaseObjectSearcher.ObjectExplorerManager manager = new DatabaseObjectSearcher.ObjectExplorerManager();
-
-        public static MSSQLController Current
+        public static MSSQLController Instance
         {
-            get
-            {
-                return currentInstance;
-            }
+            get;
+            private set;
         }
 
-        public EnvDTE.Window SearchWindow
+        public Window SearchWindow
         {
-            get
-            {
-                return toolWindow;
-            }
+            get;
+            private set;
         }
 
-        public void CreateAddinWindow(AddIn addinInstance)
+        static MSSQLController()
         {
-            Assembly asm = Assembly.Load("HuntingDog");
-
-            // Guid id = new Guid("4c410c93-d66b-495a-9de2-99d5bde4a3b9"); // this guid doesn't seem to matter?
-            //toolWindow = CreateToolWindow("DatabaseObjectSearcherUI.ucMainControl", asm.Location, id,  addinInstance);
-
-            Guid id = new Guid("4c410c93-d66b-495a-9de2-99d5bde4a3b8"); // this guid doesn't seem to matter?
-            toolWindow = CreateToolWindow("HuntingDog.ucHost", asm.Location, id, addinInstance);
-
-            //if (_application == null)
-            //{
-            //    _application = application;
-            //}
+            Instance = new MSSQLController();
         }
 
-        [SuppressMessage("Microsoft.Security", "CA2122")]
-        private EnvDTE.Window CreateToolWindow(String typeName, String assemblyLocation, Guid uiTypeGuid, AddIn addinInstance)
+        public void CreateAddinWindow(AddIn addIn)
         {
-            Windows2 win2 = ServiceCache.ExtensibilityModel.Windows as Windows2;
-            //Windows2 win2 = applicationObject.Windows as Windows2;
+            var assemblyName = Assembly.GetExecutingAssembly().FullName;
+            var className = typeof(HuntingDog.ucHost).FullName;
+            var caption = "Hunting Dog";
+            var windowId = "Hunting Dog Tool Window";
+            Object userControl = null;
 
-            if (win2 != null)
+            var windows = ServiceCache.ExtensibilityModel.Windows as Windows2;
+
+            if (windows != null)
             {
-                Object controlObject = null;
-                Assembly asm = Assembly.GetExecutingAssembly();
-                EnvDTE.Window toolWindow = win2.CreateToolWindow2(addinInstance, assemblyLocation, typeName, "Hunting Dog", "{" + uiTypeGuid.ToString() + "}", ref controlObject);
-                EnvDTE.Window oe = null;
-
-                foreach (EnvDTE.Window w1 in addinInstance.DTE.Windows)
-                {
-                    if (w1.Caption == "Object Explorer")
-                    {
-                        oe = w1;
-
-                        //if(oe.LinkedWindows!=null)
-                        //    oe.LinkedWindows.Add(toolWindow);  
-                    }
-                }
-
-                //toolWindow.Width = oe.Width;
-                // toolWindow.SetKind((vsWindowType)oe.Kind);
-                // toolWindow.IsFloating = oe.IsFloating;
-
-                // oe.LinkedWindows.Add(toolWindow);
-
-                //Window frame = win2.CreateLinkedWindowFrame(toolWindow,oe, vsLinkedWindowType.vsLinkedWindowTypeHorizontal);
-
-                //frame.SetKind(vsWindowType.vsWindowTypeDocumentOutline);
-
-                //addinInstance.DTE.MainWindow.LinkedWindows.Add(frame);
-
-                //frame.Activate();
-                //HuntingDog.Properties.Resources.spider1.MakeTransparent(System.Drawing.Color.FromArgb(0, 255, 0));
-
-                //stdole.IPicture tabPic = Support.ImageToIPicture(img) as stdole.IPicture;
-
-                toolWindow.SetTabPicture(HuntingDog.Properties.Resources.footprint.GetHbitmap());
-                toolWindow.Visible = true;
-
-                // toolWindow.Linkable = true;
-                //  toolWindow.IsFloating = false;
-
-                //    addinInstance.DTE.MainWindow.LinkedWindows.Add(toolWindow);
-                //if (oe != null)
-                //{
-
-                //    if (toolWindow.LinkedWindowFrame == null)
-                //    {
-                //        if (oe.LinkedWindowFrame != null)
-                //        {
-                //            oe.LinkedWindowFrame.LinkedWindows.Add(toolWindow);
-                //        }
-                //        else
-                //        {
-                //            toolWindow.Left = oe.Left;
-                //            toolWindow.Top = oe.Top;
-                //            toolWindow.Width = oe.Width;
-                //            toolWindow.Height = oe.Height;
-
-                //            Window2 winFrame = (Window2)win2.CreateLinkedWindowFrame(oe, toolWindow, vsLinkedWindowType.vsLinkedWindowTypeHorizontal);
-                //        }
-                //    }
-
-                //    //winFrame.SetProperty((int)__VSFPROPID.VSFPROPID_FrameMode, VSFRAMEMODE.VSFM_MdiChild);  
-                //    //winFrame.Linkable = true;
-                //    //winFrame.IsFloating = false;
-                //}
-
-                return toolWindow;
+                SearchWindow = windows.CreateToolWindow2(addIn, assemblyName, className, caption, windowId, ref userControl);
+                SearchWindow.SetTabPicture(HuntingDog.Properties.Resources.footprint.GetHbitmap());
+                SearchWindow.Visible = true;
             }
-
-            return null;
         }
     }
 }
