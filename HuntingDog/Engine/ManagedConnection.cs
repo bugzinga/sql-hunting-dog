8c322eb2a89ec4d5c4341f7c87c0e49d846b35f5
csbubbles <devnull@localhost>
2013-04-07 23:25:36 -0700

    To previous revision: added missed class

---
+++ HuntingDog/Engine/ManagedConnection.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.UI.VSIntegration.ObjectExplorer;
+
+namespace DatabaseObjectSearcher
+{
+    [SuppressMessage("Microsoft.Design", "CA1063")]
+    public class ManagedConnection : IManagedConnection
+    {
+        public SqlOlapConnectionInfoBase Connection
+        {
+            get;
+            set;
+        }
+
+        public void Close()
+        {
+        }
+
+        [SuppressMessage("Microsoft.Design", "CA1063")]
+        public void Dispose()
+        {
+        }
+    }
+}
